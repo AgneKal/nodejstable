858be249c6bf81a9171059bd1438c343f0a4666e
AgneKal <157694253+AgneKal@users.noreply.github.com>
2024-05-21 15:34:37 +0300

    init

---
+++ src/app.ts
@@ -0,0 +1,51 @@
+
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+
+    let level = 0;
+
+    if (url != null) {
+        const parts = url.split('/');
+        level = parseInt(parts[1]);
+    }
+
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.write('<!DOCTYPE html>');
+    res.write('<html>');
+
+    res.write('<head>');
+    res.write('<title>Daugybos lentelė</title>');
+    res.write('</head>');
+
+    res.write('<body>');
+
+    res.write(`<a href="/10">10%</a>&nbsp;`);
+    res.write(`<a href="/30">30%</a>&nbsp;`);
+    res.write(`<a href="/50">50%</a>&nbsp;`);
+
+    res.write(`<h1>Daugybos lentelė su ${level}% paslėptų laukų</h1>`);
+    res.write('<table border="1">');
+
+    const probability = level / 100;
+
+    for (let i = 1; i <= 10; i++) {
+        res.write('<tr>');
+        for (let j = 1; j <= 10; j++) {
+            if (Math.random() < probability) {
+                res.write('<td></td>');
+            } else {
+                res.write(`<td>${i * j}</td>`);
+            }
+        }
+        res.write('</tr>');
+    }
+
+    res.write('</table>');
+    res.write('</body>');
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(2999, 'localhost');
